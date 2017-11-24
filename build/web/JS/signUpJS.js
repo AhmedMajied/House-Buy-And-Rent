8d1744f675ba0a51f1775051e170054703c5626e
Ahmed Majeed <aabdelmeged70@yahoo.com>
2017-11-24 13:28:53 +0200

    CAPTCHA is finished

--- build/web/JS/signUpJS.js
+++ build/web/JS/signUpJS.js
@@ -1,9 +1,29 @@
-document.getElementById("submitBtn").disabled = true;
+$(document).ready(function(){
+    $("#signUpButton").attr('disabled', 'disabled');
+    
+});
+
+function validateInputs()
+    {
+        var Name=$("#user").val();
+        var password=$("#pass").val();
+        var mail=$("#email").val();
+        var phoneNumber=$("#phoneNumber").val();
+        valid=false;
+
+        if(Name.length<4)
+        {
+            $("#userWarn").html("user name must have at least 4 characters");
+            valid=false;
+        }
+        // check if name or mail already exists in the system
+
+        return valid ;
+    }
 
 function validateCaptcha(){
-    document.getElementById("submitBtn").disabled = false;
-}
-
+        $("#signUpButton").removeAttr('disabled');
+    }
 
 
 
---
+++ build/web/js/loginJS.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    
+});
+
+function validateInputs()
+    {
+        var Name=$("#user").val();
+        var password=$("#pass").val();
+        valid=true;
+        if(Name.length<4)
+        {
+            $("#warning").html("Invalid UserName or Password");
+            valid=false;
+        }
+        return valid;
+    }
