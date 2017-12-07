267a58c2e1b2e94bd993f3e4b9ed849f385586c8
MariamAshrafFekry <mariamashraf096@gmail.com>
2017-12-07 21:18:58 +0200

    addPhoto

---
+++ src/java/DBModels/AdvertisementDBModel.java
@@ -0,0 +1,139 @@
+package DBModels;
+
+import java.util.*;
+import Models.*;
+import config.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AdvertisementDBModel {
+
+    public AdvertisementDBModel() {
+    }
+    
+    public Vector<BuildingStatus> retrieveAllStatuses() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        Connection conn = DBConfig.getConnection();
+        Statement stmt= conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM BuildingStatuses");
+        Vector<BuildingStatus> ret = new Vector<>();
+        while(rs.next()) {
+        	ret.add(new BuildingStatus(rs.getInt("ID"),rs.getString("Name")));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+        return ret;
+    }
+    
+    
+    public Vector<BuildingType> retrieveAllTypes() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        Connection conn = DBConfig.getConnection();
+        Statement stmt= conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM BuildingTypes");
+        Vector<BuildingType> ret = new Vector<>();
+        while(rs.next()) {
+        	ret.add(new BuildingType(rs.getInt("ID"),rs.getString("Name")));
+        }
+        rs.close();
+        stmt.close();
+        conn.close();
+        return ret;
+    }
+    
+    public Vector<Advertisement> retrieveAllAds() {
+        // TODO implement here
+        return null;
+    }
+
+    public void saveNewAd(Advertisement ad) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        Connection conn = DBConfig.getConnection();
+        Statement stmt= conn.createStatement();
+        stmt.executeUpdate("INSERT INTO Advertisement(Title,Size,Floor,Description,Latitude,Longitude,AdvertisorID,AdType,buildingStatus,buildingType) VALUES('"
+                            +ad.getTitle()+"',"+ad.getBuildingSize()+","+ad.getBuildingFloor()+",'"
+                            +ad.getDescription()+"',"+ad.getLatitude()+","+ad.getLongitude()+","+ad.getAdvertiserID()+",'"
+                            +ad.getAdType()+"',"+ad.getStatus()+","+ad.getType()+")"
+        );
+        stmt.close();
+        conn.close();
+    }
+
+    public boolean updateAd(Advertisement ad) {
+        // TODO implement here
+        return false;
+    }
+
+    public boolean deleteAd(int adID) {
+        // TODO implement here
+        return false;
+    }
+
+    public Vector<Advertisement> retrieveUserAds(int adID) {
+        // TODO implement here
+        return null;
+    }
+
+    public boolean rateAd(int userID, int adID, double value) {
+        // TODO implement here
+        return false;
+    }
+
+    public boolean commentOnAd(int userID, int adID, String commentText) {
+        // TODO implement here
+        return false;
+    }
+
+    public Advertisement retrieveAd(int id) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        Connection conn = DBConfig.getConnection();
+        Statement stmt= conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Advertisements WHERE ID = "+id);
+        rs.next();
+        Advertisement ret = new Advertisement();
+        ret.setID(rs.getInt("ID"));
+        ret.setTitle(rs.getString("Title"));
+        ret.setDescription(rs.getString("Description"));
+        ret.setBuildingSize(rs.getInt("buildingSize"));
+        ret.setBuildingFloor(rs.getInt("buildingFloor"));
+        ret.setLatitude(rs.getDouble("Latitude"));
+        ret.setLongitude(rs.getDouble("Longitude"));
+        ret.setAdvertiserID(rs.getInt("AdvertisorID"));
+        int buildingType=rs.getInt("buildingType");
+        int buildingStatus=rs.getInt("buildingStatus");
+        ret.setAdType(rs.getString("AdType"));
+
+        ResultSet rs2 = stmt.executeQuery("SELECT * FROM BuildingTypes WHERE ID = "+buildingType);
+        if(rs2.next())ret.setType(new BuildingType(rs2.getInt("ID"),rs2.getString("Name")));
+        ResultSet rs3 = stmt.executeQuery("SELECT * FROM BuildingStatuses WHERE ID = "+buildingStatus);
+        if(rs3.next())ret.setStatus(new BuildingStatus(rs3.getInt("ID"),rs3.getString("Name")));
+        
+        ResultSet rs4 = stmt.executeQuery("SELECT Photo FROM BuildingPhotos WHERE AdID = "+ret.getID());
+        Vector<Blob> pics= new Vector<>();
+        while(rs4.next()){
+            pics.add(rs4.getBlob("Photo"));
+        }
+        ret.setPhotos(pics);
+        rs.close();
+        stmt.close();
+        conn.close();
+        return ret;
+    }
+
+    public void addPhoto(File f,int adID) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, FileNotFoundException {
+        Connection connection = DBConfig.getConnection();
+        PreparedStatement psmnt = connection.prepareStatement("INSERT INTO BuildingPhotos(AdID,Photo)VALUES(?,?)");
+        FileInputStream fis = new FileInputStream(f);
+        psmnt.setInt(1, adID);
+        psmnt.setBlob(2, (InputStream) fis, (int) (f.length()));
+        psmnt.executeUpdate();
+        psmnt.close();
+        connection.close();
+    }
+
+}
---
+++ web/jsp/profile.jsp
@@ -0,0 +1,109 @@
+<%-- 
+    Document   : profile
+    Created on : Nov 19, 2017, 11:27:20 PM
+    Author     : MariamAshraf
+--%>
+
+<%@page import="java.util.Base64"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import ="Models.User" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../css/headerStyle.css">
+        <link rel="stylesheet" href="../css/profileStyle.css">
+        <script src="../js/jquery-3.1.1.min.js"></script>
+        <script src="../js/profileJs.js"></script>
+
+        <title>User Profile</title>
+    </head>
+    <body>
+        <%
+            HttpSession currentSession=request.getSession(true);
+            User user=new User();
+            if(currentSession.getAttribute("User")!=null)
+            {
+                user=(User)currentSession.getAttribute("User");
+            }
+            else
+            {
+                response.sendRedirect("/IA_Project/");
+            }
+        %>
+        <header>
+            <div id="navBar">
+                <a id="active" href ="Home.jsp">Home</a>
+                <a href="#">My Advertisments</a>
+                <a href="#">About</a>
+                <span id="search"></span>
+                <input type="text"name="search"id="searchText" placeholder="search field"/>
+                <a href="#"id="notification"></a>
+                <a href="profile.jsp">MyProfile</a>
+                <a href="../index.html">LogOut</a>
+            </div>
+        </header>
+        <section>
+            <br><br>
+            <label id="userName"><%=user.getUsername()%></label>
+            <br><br>
+            <fieldset id="fieldSet">
+                <legend>Your Photo</legend>
+                 <%  if (user.getPicture()!=null)
+                    {
+                      String url = "data:image/png;base64," + Base64.getEncoder().encodeToString(user.getPicture().getBytes(1,(int)user.getPicture().length()));
+                 %>
+                      <img src="<%=url%>"name="image"id="userImage">
+                    <%} else 
+                    {%>
+                         <img src="../images/userImage.png" name="image" id="userImage">
+                    <%}%>
+                <form action="/IA_Project/UserController?action=addPhoto" method="POST" enctype="multipart/form-data">
+                    <input type="file" accept="image/*" id="addPhoto" name="file" onchange="loadFile(event)">
+                    <input type="submit" value="Save Photo"id="savePhoto">
+                </form>
+            </fieldset>
+            <br><br>
+            <% if (user.getPicture()!=null)
+            {%>
+            
+                <form action="/UserController?action=deletePhoto"id="deleteImage">
+                  <input type="submit"value="Delete Image"id="deletephoto">
+                </form>
+              <%}%>
+              
+                <br><br>
+              
+              <button id="addPhone" onclick="displayPhone();">Change Phone</button>
+              <fieldset id="Phone">
+                  <form action="/IA_Project/UserController?action=addPhone" method="post">
+                        <span id="phone"></span>
+                        <input type="text" pattern="^[0-9]+" placeholder="Add Your Phone" name="phoneNumber" id="phoneNumber">
+                        <br><br>
+                        <input type="submit" id="submit" value="add phone Number"/> 
+                    </form>
+              </fieldset>              
+                <%if(user.getPhone()!=null){%>
+                <form id="deletePhone" action="/IA_Project/UserController?action=deletePhone" method="post">
+                    <br>
+                    <input type="submit" id="submit1" value="Delete Phone"/> 
+                </form>
+              <br>
+              <%}%>
+              <br><br>
+               <button id="changePassword" onclick="displayFields();">ChangePassword</button>
+                <fieldset id="password">
+                    <form action="/IA_Project/UserController?action=changePassword" method="post"id="passwordForm">
+                          <span id='iPass'></span>
+                          <input type="password" required placeholder='old Password'name="oldPassword" id="oPassword"><br><br>
+                          <span id='iPass'></span>
+                          <input type="password" required placeholder='new Password'name="newPassword" id="nPassword"><br><br>
+                          <div id="valid"></div>
+                          <input type="submit" id="submit" value="change Password"/> 
+                      </form>
+                 </fieldset> 
+              
+        </section>
+    </body>
+</html>
